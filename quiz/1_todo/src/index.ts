dd2a0c217b5489c811e3a76099813357e216cbbe
oreeyo00 <oreeyo00@gmail.com>
2024-02-19 22:53:27 +0900

    Todo typing 과제

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,8 +1,23 @@
-let todoItems;
+// typing(타이핑) => 타입이 정의되지않은 코드에 타입을 입혀주는 행위
+
+// any 타입 => 자바스크립트의 원래 성질인 
+// 실행하는 시점에 타입을 구분해서 타입을 할당해주는 관점에서 any가 있다고 보면 된다.
+// 타입 모르면 any가 치트키인 느낌..?
+
+
+// 할 일 항목을 위한 인터페이스 정의
+interface TodoItem {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems:  Array<TodoItem>;
+
 
 // api
 function fetchTodoItems() {
-  const todos = [
+  const todos : Array<TodoItem> = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
     { id: 3, title: '스크립트', done: false },
@@ -11,20 +26,20 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): Array<TodoItem> {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo : TodoItem) {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index : number) {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index : number, todo : TodoItem) {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
