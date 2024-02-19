270c0fa1063464e18501b97c8a7a8e132fadde4c
oreeyo00 <oreeyo00@gmail.com>
2024-02-19 23:42:48 +0900

    Typing ver 1.0

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -4,20 +4,16 @@
 // 실행하는 시점에 타입을 구분해서 타입을 할당해주는 관점에서 any가 있다고 보면 된다.
 // 타입 모르면 any가 치트키인 느낌..?
 
+// let todoItems : object[];      // object[] => object로 이루어진 배열이라는 뜻.
 
-// 할 일 항목을 위한 인터페이스 정의
-interface TodoItem {
-  id: number;
-  title: string;
-  done: boolean;
-}
-
-let todoItems:  Array<TodoItem>;
+// showComplted 함수에서 done도 타입을 지정해줘야하기 때문에, 이렇게 일일히 타입 지정을 해줌,
+// 아마 인터페이스 배우기 전이라 일부러 이렇게하는 느낌.
+let todoItems : {id: number, title: string, done: boolean}[]; 
 
 
 // api
 function fetchTodoItems() {
-  const todos : Array<TodoItem> = [
+  const todos : {id: number, title: string, done: boolean}[] = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
     { id: 3, title: '스크립트', done: false },
@@ -26,40 +22,56 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos(): Array<TodoItem> {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo : TodoItem) {
+// 함수의 반환 값이 없을 땐 , 명시적으로 return 타입을 void로 지정해줘야 한다.
+function addTodo(todo : {id: number, title: string, done: boolean}) : void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index : number) {
+function deleteTodo(index : number) : void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index : number, todo : TodoItem) {
+function completeTodo(index : number, todo : {id: number, title: string, done: boolean}) : void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo() : object{
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted() : object[] {
   return todoItems.filter(item => item.done);
+/*
+  todoItems.filter(function(item){
+    if(item.done === true){
+      return item;
+    }
+  })
+*/
+
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems() : void {
+  
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+
+const item4 = {id: 4, title: 'item4', done: false }
+addTodo(item4)
+addTodo({id: 5, title: 'item5', done: false })  
+
+
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log() : void {
   console.log(todoItems);
 }
 
