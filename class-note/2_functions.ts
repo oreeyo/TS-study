425a8c80bd0f166cad4f078f0151935d05d3cd98
oreeyo00 <oreeyo00@gmail.com>
2024-02-19 20:34:52 +0900

    ts에서 함수 타입 선언 방법

---
+++ class-note/2_functions.ts
@@ -0,0 +1,17 @@
+// 함수의 파라미터에 타입을 정의하는 방식이다.
+function _sum(a:number, b:number) {
+    return a+b;
+}
+
+_sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add():number {
+    return 10;
+}
+
+// 함수에 타입을 정의하는 방식
+function __sum(a:number, b:number):number {
+    return a+b;
+}
+
