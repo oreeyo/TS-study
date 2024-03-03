84463e788bea11c5ab357c418861d20be9aabd51
oreeyo00 <oreeyo00@gmail.com>
2024-03-04 08:06:16 +0900

    enum  활용 사례

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -5,3 +5,24 @@ enum Shoes {
 
 const myShoes = Shoes.nike
 console.log(myShoes)
+
+enum Answer {
+    Yes = 'y',
+    No = 'n'
+}
+
+function askQuestion (answer : Answer) {
+    if(answer === Answer.Yes) {
+        console.log('정답입니다')
+    }
+    
+    if(answer === Answer.No) {
+        console.log('오답입니다.')
+    }
+}
+
+askQuestion(Answer.Yes)
+
+
+
+// askQuestion('정답')
